ea83d3ed3dae0f0c309f8ffb1462846364ed8bab
Marwane <manbou92@hotmail.fr>
2021-07-29 04:12:38 +0200

    transactions devise de paiement

---
+++ client/src/config/transactions.config.js
@@ -0,0 +1,34 @@
+export default {
+    currencies: [
+        {
+            currency: "EUR",
+            name: "Euros",
+            sign: '€'
+        },
+        {
+            currency: "USD",
+            name: "American Dollars",
+            sign: '$'
+        },
+        {
+            currency: "GBP",
+            name: "Pounds",
+            sign: '£'
+        },
+        {
+            currency: "CAD",
+            name: "Canadian Dollars",
+            sign: '$CAD'
+        },
+        {
+            currency: "AUD",
+            name: "Australian Dollars",
+            sign: '$AUS'
+        },
+    ],
+    status: {
+        PENDING: 'PENDING',
+        CONFIRMED: 'CONFIRMED',
+        CANCELED: 'CANCELED',
+    }
+};
