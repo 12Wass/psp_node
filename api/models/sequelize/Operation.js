d516f214b95af2a925746bf075849c594edb184a
yassansplus <yassine.bousaidi@hotmail.com>
2021-07-29 12:49:40 +0200

    Adding package-lock.json on multiple folders + adding models folder

---
+++ api/models/sequelize/Operation.js
@@ -0,0 +1,35 @@
+import sequelize from '../../../../buy-your-language-full/api/lib/sequelize';
+import { DataTypes, Model } from 'sequelize';
+import Transaction from './Transaction';
+
+class Operation extends Model {}
+
+Operation.init({
+    amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.ENUM(["WAITING", "CANCELED", "COMPLETED", "FAILED"]),
+        allowNull: false,
+    },
+    card: {
+        type: DataTypes.STRING, 
+        get: function() {
+            return JSON.parse(this.getDataValue('card'));
+        }, 
+        set: function(val) {
+            return this.setDataValue('card', JSON.stringify(val || {}));
+        },
+    },
+    type: {
+        type: DataTypes.ENUM(["PAYMENT", "REFUND"])
+    }},
+    {
+        sequelize,
+        modelName: "Operation",
+        paranoid: true,
+    }
+);
+
+export default Operation;
---
+++ api/models/sequelize/index.js
@@ -0,0 +1,28 @@
+import sequelize from '../../../../buy-your-language-full/api/lib/sequelize';
+import User from './User';
+import Transaction from './Transaction';
+import Operation from './Operation';
+import './hooks';
+
+// Relations
+Transaction.belongsTo(User);
+Transaction.hasMany(Operation);
+
+// Relations
+Operation.belongsTo(Transaction);
+
+// Relations
+User.hasMany(Transaction);
+
+// sequelize
+//   .sync({ force: true })
+//   .then((result) => console.log("All models were synchronized successfully."))
+//   .catch((result) => console.error(result, "Error with models synchronization"));
+
+export default {
+    sequelize,
+    User,
+    Transaction,
+    Operation,
+};
+  
