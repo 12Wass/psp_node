e312b5e3e1101c15376a29446feeb3cd2b0672cd
Marwane <manbou92@hotmail.fr>
2021-07-29 04:07:19 +0200

    Add listining language de programmation

---
+++ client/src/config/listings.js
@@ -0,0 +1,68 @@
+import PHP from '../assets/php.svg';
+import Javascript from '../assets/javascript.svg';
+import C from '../assets/c.svg';
+import CPP from '../assets/cpp.svg';
+import NodeJS from '../assets/nodejs.svg';
+import Assembly from '../assets/assembly.svg';
+import Ruby from '../assets/ruby.svg';
+import R from '../assets/r.svg';
+
+
+export default [
+    {
+        name: "PHP",
+        description: "PHP is a server side scripting language that is embedded in HTML. Very used!",
+        price: 15,
+        quantity: 0,
+        image: PHP,
+    },
+    {
+        name: "Javascript",
+        description: "JavaScript is a dynamic computer programming language. It is lightweight and most commonly used as a part of web pages, whose implementations allow client-side script to interact with the user and make dynamic pages",
+        price: 27,
+        quantity: 0,
+        image: Javascript,
+    },
+    {
+        name: "C",
+        description: "The C programming language is a computer programming language. Don't use it. Like, seriously...",
+        price: 7,
+        quantity: 0,
+        image: C,
+    },
+    {
+        name: "Ruby on Rails",
+        description: "Ruby on Rails is an open-source web development framework, which provides Ruby developers a timesaving alternative to develop code. It is a collection of code libraries !",
+        price: 120,
+        quantity: 0,
+        image: Ruby,
+    },
+    {
+        name: "C++",
+        description: "C++ is a statically typed, compiled, general-purpose, case-sensitive, free-form programming language that supports procedural, object-oriented, and generic programming. Try not to use it though..",
+        price: 18,
+        quantity: 0,
+        image: CPP,
+    },
+    {
+        name: "Node.js",
+        description: "Node.js is an open source development platform for executing JavaScript code server-side. Node is useful for developing applications that require a persistent connection from the browser to the server and is often used for real-time applications such as chat, news feeds and web push notifications.",
+        price: 57,
+        quantity: 0,
+        image: NodeJS,
+    },
+    {
+        name: "Assembly",
+        description: "In computer programming, assembly language, sometimes abbreviated asm, is any low-level programming language in which there is a very strong correspondence between the instructions in the language and the architecture's machine code instructions. You'd rather die than use this.",
+        price: 3,
+        quantity: 0,
+        image: Assembly,
+    },
+    {
+        name: "R",
+        description: "R is an open-source language that is essentially a different version of the S language. Much of the code that developers write for S runs on R without modification. You like letters? Go ahead.",
+        price: 12,
+        quantity: 0,
+        image: R,
+    },
+];
